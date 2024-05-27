581d47ec9c25ec8fb7b12ad4a9ce1344ef6944a1
Mohammed Ihtisham <ihtishammohammed2004@gmail.com>
2024-05-27 16:45:44 -0400

    Created Middleware and Error Handler Function

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import { stat } from "fs";
 
 dotenv.config() 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,9 +14,22 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json()); 
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
 );
 
-app.use('/api/user', userRouter);
+app.use('/api/user', userRouter); 
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500; 
+    const message = err.message || "Internal Sever Error";
+    return res.status(statusCode).json({
+        success: false, 
+        statusCode,
+        message,
+    });
+});
